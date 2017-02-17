d7011ea85a7854607a8f914e20476b479caec731
Little Mike <code@littlemike.co.uk>
2017-02-17 13:59:15 +0000

    Task 37

--- src/v_builders/_37_StringAndMapBuilders.kt
+++ src/v_builders/_37_StringAndMapBuilders.kt
@@ -28,12 +28,17 @@ fun todoTask37(): Nothing = TODO(
     """
 )
 
+fun <K, V> buildMap(mapBuilder: MutableMap<K, V>.() -> Unit): Map<K, V> {
+    val map = HashMap<K, V>()
+    mapBuilder.invoke(map)
+    return map
+}
+
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
